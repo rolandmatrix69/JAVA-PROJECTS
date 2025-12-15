f923b0862981296ba481c0cece56cbcd8494890d
Evans Roland <evidenceroland20@gmail.com>
2025-12-15 23:40:28 +0100

    Variables Study

---
+++ Variables/Variables.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myAge = 30;
+        myAge = 21;
+        int danielAge = myAge;
+        System.out.println(danielAge);
+    }
+}
