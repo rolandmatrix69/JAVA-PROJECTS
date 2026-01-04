5747e241a753915d6d8697e503e66899b5496d64
Evans Roland <evidenceroland20@gmail.com>
2026-01-04 22:38:11 +0100

    Old method of array decl

---
+++ Arrays/LearnArrays.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class LearnArrays {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 5;
+        numbers[1] = 10;
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
