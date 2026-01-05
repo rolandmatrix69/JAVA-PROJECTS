d3fa991514013d4d5e1d7683902616917ddef8b4
Evans Roland <evidenceroland20@gmail.com>
2026-01-05 03:36:58 +0100

    constants

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -6,5 +6,9 @@ public class Variables {
         myAge = 21;
         int danielAge = myAge;
         System.out.println(danielAge);
+
+        final float PI = 3.142F;
+        // Constants Declaration
+        System.out.println(PI);
     }
 }
