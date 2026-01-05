0087a0efea20fdb8bbb40ece09c8ae8ccd509790
Evans Roland <evidenceroland20@gmail.com>
2026-01-05 04:04:43 +0100

    Arithmetic operators

---
+++ ArithmeticExpressions/ArEX.java
@@ -0,0 +1,26 @@
+package ArithmeticExpressions;
+
+public class ArEX {
+    public static void main(String[] args) {
+        int result = 10 + 3;
+        System.out.println(result);
+        // Arithmetic operators include + - * /
+        int x = 1;
+        x++;
+        System.out.println(x);
+        // Increment operators
+        int y = 2;
+        // int z = y++;
+        // Here z is first the initial value of y
+        int z = ++y;
+        // Here y is incremented by 1 before assigned to z
+        System.out.println(y);
+        System.out.println(z);
+
+        int w = 3;
+        w += 3;
+        System.out.println(w);
+
+    }
+
+}
