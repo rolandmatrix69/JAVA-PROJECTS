6116acdac0eff2a25866d89839d381efe3a74e6e
Evans Roland <evidenceroland20@gmail.com>
2026-01-25 13:58:07 +0100

    Fizzbuzz Challenge

---
+++ FizzBUzz/Fizzbuzz.java
@@ -0,0 +1,23 @@
+package FizzBUzz;
+
+import java.util.Scanner;
+
+public class Fizzbuzz {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number of your choice: ");
+        int number = input.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FIzzbuzz");
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
+
+    }
+
+}
