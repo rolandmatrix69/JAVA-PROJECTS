ddd117cab82a6a16edd6c4bd53bf5f246a6d14be
Evans Roland <evidenceroland20@gmail.com>
2026-02-04 16:09:40 +0100

    Methods

---
+++ Methods/cleanCoding.java
@@ -0,0 +1,18 @@
+package Methods;
+
+public class cleanCoding {
+    public static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static int add(int a, int b) {
+        return (a + b);
+
+    }
+
+    public static void main(String[] args) {
+        greet("John");
+        System.out.println(add(3, 5));
+
+    }
+}
