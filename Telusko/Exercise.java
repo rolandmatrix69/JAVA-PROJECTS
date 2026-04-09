8739894d6b9f74c51ee487d92713f9d91c4443a9
Evans Roland <evidenceroland20@gmail.com>
2026-04-09 15:44:35 +0100

    Telusko Classes

---
+++ Telusko/Exercise.java
@@ -0,0 +1,19 @@
+package Telusko;
+
+public class Exercise {
+  public static void main(String[] args) {
+
+    // write code here
+    int a = 20, b = 10;
+
+    boolean result1 = a > b;
+    boolean result2 = a > b && b > 0;
+
+    System.out.println("Addition: " + (a + b));
+    System.out.println("Subtraction: " + (a - b));
+    System.out.println("Multiplication: " + a * b);
+    System.out.println("Division: " + a / b);
+    System.out.println("Is a greater than b? " + result1);
+    System.out.println("Is a > b and b > 0? " + result2);
+  }
+}
